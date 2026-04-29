1f3da6e43f241aaf7d0ce9b9b763cae239717774
SohanKaushik <sohangamedev@gmail.com>
2026-04-29 20:31:20 +0530

    mvp matrices

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,18 @@
 #include <GLFW/glfw3.h>
 #include "buffers/gl_buffers.h"
 #include "window/window.h"
+#include <glm/glm.hpp>
+#include <cmath>
+
+#include <glm/gtc/matrix_transform.hpp>
 
 // Vertex Shader source code
 const char* vs_src = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"uniform mat4 MVP;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
+"   gl_Position = MVP * vec4(aPos, 1.0);\n"
 "}\0";
 //Fragment Shader source code
 const char* fragmentShaderSource = "#version 330 core\n"
@@ -42,6 +47,7 @@ int main() {
     // viewport
     glViewport(0, 0, 800, 500);
 
+    float aspect = 800.0f / 500.0f;
     
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vs, 1, &vs_src, NULL);
@@ -66,28 +72,72 @@ int main() {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-
+        
+        // current shader
         glUseProgram(shaderp);
-        //line1->draw();
-        //line2->draw();
-        glPointSize(2.0f);
 
-        std::vector<float> point1 = { 0,0,0 };
-        std::vector<float> point2 = { 0,0.5,0 };
+        glm::mat4 view = glm::mat4(1.0f); // pan, zoom, rotate 
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)aspect, 0.1f, 1000.0f); // projection
 
-        std::vector<float> point3 = { 0.5,0,0 };
-        std::vector<float> point4 = { 0,-0.5,0 };
 
-        std::vector<float> point5 = { 0,0,0 };
 
-        std::vector<float> points;
-        points.insert(points.end(), point1.begin(), point1.end());
-        points.insert(points.end(), point2.begin(), point2.end());
-        points.insert(points.end(), point3.begin(), point3.end());
-        points.insert(points.end(), point4.begin(), point4.end());
-        points.insert(points.end(), point5.begin(), point5.end());
 
-        auto lines = std::make_unique<geometry>(points, rdrtype::CONNECT_LINES);
+
+
+
+
+
+        glm::mat4 model = glm::mat4(1.0f);
+
+        float time = glfwGetTime() * 500;
+        model = glm::translate(
+            model,
+            glm::vec3(0, 0, -1) // move right & up
+        );
+
+        model = glm::rotate(model, glm::radians(time), glm::vec3(0.0f, 1.0f, 0.0f));
+
+
+        glm::mat4 MVP = projection * view * model;
+
+        std::vector<glm::vec3> points;
+
+        int step = 4;
+        float radius = 0.2f;
+
+        for (size_t i = 0; i <= step; i++)
+        {
+            float angle = (2.0f * glm::pi<float>() * i) / step;
+            points.push_back(glm::vec3(radius * cos(angle), radius * sin(angle), 0.0f));
+        }
+
+        // convert vec3 to float
+        std::vector<float> floatVector;
+        floatVector.reserve(points.size() * 3);  // Reserve space for efficiency
+        for (const auto& vec : points) {
+            floatVector.push_back(vec.x);
+            floatVector.push_back(vec.y);
+            floatVector.push_back(vec.z);
+        }
+
+        unsigned int loc = glGetUniformLocation(shaderp, "MVP");
+        glUniformMatrix4fv(loc, 1, GL_FALSE, &MVP[0][0]);
+        
+
+        /*
+        for (auto& obj : objects)
+        {
+            glm::mat4 model = obj.getModel();
+            glm::mat4 MVP = projection * view * model;
+
+            glUniformMatrix4fv(loc, 1, GL_FALSE, &MVP[0][0]);
+
+            obj.draw();
+        }
+        */
+
+        auto lines = std::make_unique<geometry>(floatVector, rdrtype::CONNECT_LINES);
+        auto line2 = std::make_unique<geometry>(floatVector, rdrtype::CONNECT_LINES);
 
         lines->draw();
 
